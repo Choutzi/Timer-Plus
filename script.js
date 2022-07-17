6bd3c934c1c2de4184feff6517eb2a5ab18fcdfa
Choutzi <sylvaingarrido1@gmail.com>
2022-07-17 20:32:36 +0200

    Clean du code suivant les prescription javascript

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ const paramBtn = document.getElementById("display_param");
 const divTimer = document.getElementById("timer");
 const divParam = document.getElementById("param");
 const listVue = document.getElementById("list-actions");
-var isPaused = true;
+let isPaused = true;
 const listActions = new Array();
+let idCollapseCount = 0;
 
-/*
+/**
  * Une Action définie par son nom et son temps en secondes.
  * Une Action peut être composée de sous-actions
  */
@@ -23,7 +24,7 @@ class Action {
     toString() { return `${this.name}` }
 }
 
-/*
+/**
  * Buffer d'une Action permettant de mettre à jour les objets graphique du timer
  */
 class ActionTimer {
@@ -33,20 +34,23 @@ class ActionTimer {
         this.currentIndex = 0;
     }
 
+    /**
+     * Lance le décompte de l'action ou des subActions
+     */
     launch() {
         if (this.currentIndex < this.action.actions.length - 1) {
-            var nxtChild = this.getNextChild();
+            let nxtChild = this.getNextChild();
             this.currentIndex++;
             new ActionTimer(nxtChild, this).launch();
         } else {
             currentName.innerText = this.action.name;
-            var preced = this.parent;
+            let preced = this.parent;
             while (preced != null) {
                 currentName.innerText = preced.action.name + " - " + currentName.innerText;
                 preced = preced.parent;
             }
             if (this.parent != null) {
-                var nextBrother = this.parent.getNextChild()
+                let nextBrother = this.parent.getNextChild()
                 if (nextBrother != null) {
                     nextName.innerText = nextBrother.name;
                     nextTimer.innerText = getTimer(nextBrother.time);
@@ -58,25 +62,40 @@ class ActionTimer {
                 nextName.innerText = "END";
                 nextTimer.innerText = "";
             }
-
-            var temps = this.action.time;
-            var interval = setInterval(() => {
-                if (!isPaused) {
-                    currentTimer.innerText = getTimer(temps);
-                    if (temps <= 0) {
-                        clearInterval(interval);
-                        if (this.parent != null)
-                            this.parent.launch();
-                    } else
-                        temps = temps - 1;
-                }
-            }, 1000)
+            this.launchInterval();
         }
     }
 
+    /**
+     * Lance le décompte et modifi les objets graphiques liés
+     */
+    launchInterval() {
+        let temps = this.action.time;
+        let interval = setInterval(() => {
+            if (!isPaused) {
+                currentTimer.innerText = getTimer(temps);
+                if (temps <= 0) {
+                    clearInterval(interval);
+                    if (this.parent != null)
+                        this.parent.launch();
+                } else
+                    temps = temps - 1;
+            }
+        }, 1000)
+    }
+
+    /**
+     * Récupère le nextChild
+     * @returns {Action} next child
+     */
     getNextChild() { return this.action.actions[this.currentIndex] }
 }
 
+/**
+ * Permet de formater des secondes au format HH:mm
+ * @param {int} temps le temps en secondes 
+ * @returns {String} de format HH:mm
+ */
 function getTimer(temps) {
     let minutes = parseInt(temps / 60, 10);
     let secondes = parseInt(temps % 60, 10);
@@ -87,33 +106,41 @@ function getTimer(temps) {
     return `${minutes}:${secondes}`;
 }
 
-
-pauseElement.addEventListener("click", () => {
+/**
+ * Met en pause ou reprend le décompte sélectionné
+ */
+function onPause() {
     isPaused = !isPaused;
     pauseElement.innerText = isPaused ? "Start" : "Pause";
-});
+}
 
+/**
+ * Charge la liste d'actions sélectionnable
+ */
 function loadListe() {
     listActions.forEach(action => listVue.appendChild(htmlAction(action)))
 }
 
-var idCollapseCount = 0;
-
+/**
+ * Génère la partie graphique de modification des actions
+ * @param {Action} action 
+ * @returns {div} la div cotenant les input de modification de l'action et ses sub actions
+ */
 function htmlAction(action) {
-    var div = document.createElement("div");
+    let div = document.createElement("div");
     div.setAttribute("class", "list-group-item");
     div.setAttribute("draggable", "true");
-    var inputName = document.createElement("input");
+    let inputName = document.createElement("input");
     inputName.setAttribute("type", "text");
     inputName.value = action.name;
-    var inputTime = document.createElement("input");
+    let inputTime = document.createElement("input");
     inputTime.setAttribute("type", "time");
     inputTime.value = getTimer(action.time);
     div.appendChild(inputName);
     div.appendChild(inputTime);
     if (action.actions.length > 0) {
-        var idCollapse = "collapse_" + `${idCollapseCount++}`;
-        var btnCollapse = document.createElement("button");
+        let idCollapse = "collapse_" + `${idCollapseCount++}`;
+        let btnCollapse = document.createElement("button");
         btnCollapse.setAttribute("class", "btn btn-primary");
         btnCollapse.setAttribute("type", "button");
         btnCollapse.setAttribute("data-bs-toggle", "collapse");
@@ -121,10 +148,10 @@ function htmlAction(action) {
         btnCollapse.setAttribute("aria-expanded", "false");
         btnCollapse.setAttribute("aria-controls", idCollapse);
         btnCollapse.innerText = "\\/";
-        var divCollapse = document.createElement("div");
+        let divCollapse = document.createElement("div");
         divCollapse.setAttribute("class", "collapse");
         divCollapse.setAttribute("id", idCollapse);
-        var divCard = document.createElement("div");
+        let divCard = document.createElement("div");
         divCard.setAttribute("class", "card card-body")
         divCollapse.appendChild(divCard);
 
@@ -136,9 +163,9 @@ function htmlAction(action) {
     return div;
 }
 
-var pause = new Action("Pause", 5);
-var pompes = new Action("Pompes", 10);
-var cycle = new Action("Cycle", 0);
+let pause = new Action("Pause", 5);
+let pompes = new Action("Pompes", 10);
+let cycle = new Action("Cycle", 0);
 cycle.actions.push(pompes, pause, pompes, pause, pompes, pause);
 pompes.actions.push(pause);
 listActions.push(cycle, pompes);
